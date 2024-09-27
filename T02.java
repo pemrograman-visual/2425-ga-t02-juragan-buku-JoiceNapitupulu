c851c3e0c1c622b0df02430852850845b755561c
JoiceNapitupulu <anastasyajoicen@gmail.com>
2024-09-27 10:43:34 +0700

    Telah menyelesaikan tugas

--- T02.java
+++ T02.java
@@ -1,1 +1,52 @@
-// NIM - Your Name
+// 12S24020 - Joice Anastasya Napitupulu
+// 12S24014 - Arion Dippos Pandapotan Manurung
+
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String isbn, judul, penulis, penerbit, formatbuku, kualitas;
+        int tahunterbit, stok;
+        double hargapembelian, minimummargin, rating;
+
+        isbn = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunterbit = input.nextInt();
+        penerbit = input.nextLine();
+        formatbuku = input.nextLine();
+        hargapembelian = input.nextDouble();
+        minimummargin = input.nextDouble();
+        stok = input.nextInt();
+        rating = input.nextDouble();
+        kualitas = "";
+        if (rating >= 4.7) {
+            kualitas = "Best Pick";
+        } else {
+            if (rating >= 4.5) {
+                kualitas = "Must Read";
+            } else {
+                if (rating >= 4.0) {
+                    kualitas = "Recommend";
+                } else {
+                    if (rating >= 3.0) {
+                        kualitas = "Average";
+                    } else {
+                        if (rating < 3.0) {
+                            kualitas = "Low";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + toFixed(rating,1) + "|" + kualitas);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
+
